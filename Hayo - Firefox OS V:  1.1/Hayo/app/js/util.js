264d9e39123c56c1c4e96a5f62d5f8ebd4c2ceea
Almir Stark <Almirsantos05@gamil.com>
2015-02-10 10:45:25 -0300

    as

---
+++ Hayo - Firefox OS V:  1.1/Hayo/app/js/util.js
@@ -0,0 +1,37 @@
+// adiciona formatação de número de telefone no helper do HandleBars
+Handlebars.registerHelper("formatTelefone", function(telefNumero) {
+    
+    var tamanho = telefNumero.length;
+    
+    if (tamanho <= 5) {
+        //  não formata números de emergência tais como: 191, 193, etc.
+	    return telefNumero;
+
+    }else{
+
+    	return "(" + telefNumero.substr(0,2) + ") " + telefNumero.substr(2,4) + "-" + telefNumero.substr(6,4);
+    }      
+          
+});  
+
+// cria formatos de exibição de Datas.
+var DataFormatos = {
+       completa: "DD/MM/YYYY",
+       dia_mes: "DD/MM"
+};
+
+// Deprecated since version 0.8.0 
+Handlebars.registerHelper("formataData", function(data, formato) {
+// adiciona formatação de número de telefone no helper do HandleBars
+//UI.registerHelper("formataData", function(datatime, formato) {
+  
+  if (moment) {
+    f = DataFormatos[formato];
+
+    return moment(data).format(f);
+  }
+  else {
+      
+      return data;
+  }
+});
